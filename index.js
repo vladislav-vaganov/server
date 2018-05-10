7ed2bbabfdb649a5a9c9929ac7e28aa59a74f857
Vladislav Vaganov <v.s-vaganov@yandex.ru>
2018-05-10 22:12:56 +0300

    'gitignore'

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/*", (req, resp) => resp.send("Hello world!"));
+app.get("/*", (req, resp) => {
+    resp.statusCode = 201;
+    resp.write("Hello world!");
+    resp.end();
+});
 
 app.listen(3000);
